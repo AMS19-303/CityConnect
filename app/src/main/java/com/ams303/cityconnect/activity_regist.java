8657b80aafceb8ab57ef0a10ec12d447960ed65b
Alex <alexandre.lourenco@ua.pt>
2019-12-04 00:42:27 +0000

    register activity

---
+++ app/src/main/java/com/ams303/cityconnect/activity_regist.java
@@ -0,0 +1,14 @@
+package com.ams303.cityconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_regist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+    }
+}
