e7e2bc3877b3e65367e79402ed8519d6922a5c28
Alex <alexandre.lourenco@ua.pt>
2019-12-04 00:23:36 +0000

    changes in profile. added features

--- app/src/main/java/com/ams303/cityconnect/data/model/User.java
+++ app/src/main/java/com/ams303/cityconnect/data/model/User.java
@@ -4,13 +4,17 @@ public class User {
     private String email;
     private String first_name;
     private String last_name;
+    private String address;
     private int nif;
+    private int phone;
 
-    public User(String email, String first_name, String last_name, int nif) {
+    public User(String email, String first_name, String last_name, String address, int nif, int phone ) {
         this.email = email;
         this.first_name = first_name;
         this.last_name = last_name;
+        this.address = address;
         this.nif = nif;
+        this.phone = phone;
     }
 
     public String getEmail() {
@@ -25,7 +29,15 @@ public class User {
         return last_name;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
     public int getNif() {
         return nif;
+    }
+
+    public int getPhone() {
+        return phone;
     }
 }
--- app/src/main/java/com/ams303/cityconnect/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ams303/cityconnect/ui/profile/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -38,22 +39,31 @@ public class ProfileFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         //textView.setText("This is profile fragment");
+        //type
+        final Switch SwitchT = root.findViewById(R.id.Switch);
+        final ImageView avatar = root.findViewById(R.id.imageView);
         final EditText Name = root.findViewById(R.id.Name);
         final EditText Email = root.findViewById(R.id.Email);
-        final EditText NIF = root.findViewById(R.id.NIF);
         final EditText Address = root.findViewById(R.id.Address);
-        final ImageView avatar = root.findViewById(R.id.imageView);
+        final EditText NIF = root.findViewById(R.id.NIF);
+        final EditText Phone = root.findViewById(R.id.Phone);
+
         final Button editButton = root.findViewById(R.id.edit);
         final Button signOutButton = root.findViewById(R.id.sign_out);
+        final Button deleteAccountButton = root.findViewById(R.id.delete);
+
+        // Blocking the switch in a position
+        SwitchT.setChecked(true);
+        SwitchT.setClickable(false);
 
 
         MainActivity main = (MainActivity) getActivity();
         String email = main.email;
 
         Map<String, User> dummy_data = new HashMap<>();
-        dummy_data.put("r.rosmaninho@ua.pt", new User("r.rosmaninho@ua.pt", "Rodrigo", "Rosmaninho", 234567890));
-        dummy_data.put("dias.eurico@ua.pt", new User("dias.eurico@ua.pt", "Eurico", "Dias", 234567890));
-        dummy_data.put("demo@ua.pt", new User("demo@ua.pt", "Utilizador", "Demonstração", 234567890));
+        dummy_data.put("r.rosmaninho@ua.pt", new User("r.rosmaninho@ua.pt", "Rodrigo", "Rosmaninho", "Rua Sao Joao", 234567890, 911111111));
+        dummy_data.put("dias.eurico@ua.pt", new User("dias.eurico@ua.pt", "Eurico", "Dias", "Rua Sao Tiago",234567890, 912222222));
+        dummy_data.put("demo@ua.pt", new User("demo@ua.pt", "Utilizador", "Demonstração", "Test", 234567890, 913333333));
 
         if(email == null || !dummy_data.containsKey(email)){
             email = "demo@ua.pt";
@@ -65,7 +75,8 @@ public class ProfileFragment extends Fragment {
         Name.setText(user.getFirst_name() + " " + user.getLast_name());
         Email.setText(email);
         NIF.setText(String.valueOf(user.getNif()));
-        //Address.setText("Rua X");
+        Address.setText(user.getAddress());
+        Phone.setText(String.valueOf(user.getPhone()));
 
         final Activity activity = this.getActivity();
         signOutButton.setOnClickListener(new View.OnClickListener() {
